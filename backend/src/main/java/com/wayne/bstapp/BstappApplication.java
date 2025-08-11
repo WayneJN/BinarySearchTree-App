e7de95eaca0b2a8bc2248090626e7101fbd6170a
WayneJN <waynejnorman@gmail.com>
2025-08-10 22:59:30 -0230

    First commit, branch structure. both backend and frontend skeletons

---
+++ backend/src/main/java/com/wayne/bstapp/BstappApplication.java
@@ -0,0 +1,13 @@
+package com.wayne.bstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BstappApplication.class, args);
+    }
+
+}
